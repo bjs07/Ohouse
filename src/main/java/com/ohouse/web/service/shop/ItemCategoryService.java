93b4835a6a806b64741dc2bac16578a10cc2ad58
JeongHeon <wasicrob27@gmail.com>
2022-07-17 22:49:23 +0900

    Add Searching itemCategoryCodeList for 판매자페이지

---
+++ src/main/java/com/ohouse/web/service/shop/ItemCategoryService.java
@@ -0,0 +1,25 @@
+package com.ohouse.web.service.shop;
+
+import com.ohouse.web.domain.shop.codes.ItemCategoryCode;
+import com.ohouse.web.domain.shop.codes.ItemCategoryCodeRepository;
+import com.ohouse.web.web.shop.dto.ItemCategoryListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ItemCategoryService {
+    private final ItemCategoryCodeRepository itemCategoryCodeRepository;
+
+    @Transactional
+    public List<ItemCategoryListResponseDto> getItemCategoryCodeListAll(){
+
+        return itemCategoryCodeRepository.findDistinctAllAsc()
+                .stream().map(ItemCategoryListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
