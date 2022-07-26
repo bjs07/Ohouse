68ee6568e81bdaf662ab93e7aed2048d92e40a9b
JeongHeon <wasicrob27@gmail.com>
2022-07-26 21:26:58 +0900

    Add temporary User & Delivery Mock class

---
+++ src/main/java/com/ohouse/web/domain/shop/orders/User.java
@@ -0,0 +1,14 @@
+package com.ohouse.web.domain.shop.orders;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+//Temperary Mockup user class
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+}
