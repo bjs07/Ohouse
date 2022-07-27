0983eabeba5155febd220fd304b2fe54e41d2842
JeongHeon <wasicrob27@gmail.com>
2022-07-28 08:03:54 +0900

    Disable user code from unfinished works

--- src/main/java/com/ohouse/web/dto/PostDto.java
+++ src/main/java/com/ohouse/web/dto/PostDto.java
@@ -1,44 +1,44 @@
-package com.ohouse.web.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.List;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Getter
-@Entity
-
-public class PostDto {
-    @Id
-    private long id;
-
-    @Column(length = 500, nullable = false)
-    private String title;
-
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String content;
-
-    private String writer;
-
-    @Column(columnDefinition = "integer default 0")
-    private int view;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "email")
-    private UserDto userDto;
-
-    @OneToMany(mappedBy = "postDto",fetch = FetchType.EAGER,cascade = CascadeType.REMOVE)
-    @OrderBy("id asc")
-    private List<CommentDto> commentDtos;
-
-    public void update(String title, String content){
-        this.title = title;
-        this.content = content;
-    }
-}
+//package com.ohouse.web.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//@Getter
+//@Entity
+//
+//public class PostDto {
+//    @Id
+//    private long id;
+//
+//    @Column(length = 500, nullable = false)
+//    private String title;
+//
+//    @Column(columnDefinition = "TEXT", nullable = false)
+//    private String content;
+//
+//    private String writer;
+//
+//    @Column(columnDefinition = "integer default 0")
+//    private int view;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "email")
+//    private UserDto userDto;
+//
+//    @OneToMany(mappedBy = "postDto",fetch = FetchType.EAGER,cascade = CascadeType.REMOVE)
+//    @OrderBy("id asc")
+//    private List<CommentDto> commentDtos;
+//
+//    public void update(String title, String content){
+//        this.title = title;
+//        this.content = content;
+//    }
+//}
--- src/main/java/com/ohouse/web/repository/UserRepository.java
+++ src/main/java/com/ohouse/web/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.ohouse.web.repository;
-
-import com.ohouse.web.dto.UserDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserDto,String> {
-
-}
+//package com.ohouse.web.repository;
+//
+//import com.ohouse.web.dto.UserDto;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<UserDto,String> {
+//
+//}
