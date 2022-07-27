b287350686a4e3a6dfd3fa01936f2de59b7ccc30
JeongHeon <wasicrob27@gmail.com>
2022-07-28 08:00:53 +0900

    Fix user mock class

--- src/main/java/com/ohouse/web/shop/order/domain/entity/User.java
+++ src/main/java/com/ohouse/web/shop/order/domain/entity/User.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 //Temperary Mockup user class
 @Getter
 @NoArgsConstructor
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userSeq;
 }
